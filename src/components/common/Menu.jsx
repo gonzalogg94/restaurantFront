766c4551a180045dd3948a2b465244facd2ce16c
Enzo A. Heredia <eaheredia292@gmail.com>
2022-11-23 19:51:45 -0300

    se corrigen errores en el responsive de navbar

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,72 +1,76 @@
 import { Navbar, Nav, Container, Button, Col } from "react-bootstrap";
-import { Link, NavLink } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import logo from "../../components/img/Logo.png";
 import "../../components/style.css";
 
-const Menu = ({saludoNavbar, setSaludoNavbar, usuarioLogueado, setUsuarioLogueado}) => {
+const Menu = ({
+  saludoNavbar,
+  setSaludoNavbar,
+  usuarioLogueado,
+  setUsuarioLogueado,
+}) => {
+  const navegar = useNavigate();
 
-  const navegar = useNavigate()
-  
-  const logout = ()=>{
-  //norrar el localstorage
-  navegar("/");
-  localStorage.removeItem("tokenUsuario");
-  localStorage.removeItem("saludo");
-  setUsuarioLogueado({});
-  setSaludoNavbar("");
-}
+  const logout = () => {
+    //norrar el localstorage
+    navegar("/");
+    localStorage.removeItem("tokenUsuario");
+    localStorage.removeItem("saludo");
+    setUsuarioLogueado({});
+    setSaludoNavbar("");
+  };
 
   return (
     <section className="bgTransparente fuente fs-5  fw-bold">
       <Container>
         <Navbar variant="dark" expand="lg" className="row m-0 p-0">
-
           <Col lg={6} className="text-center">
-          <Navbar.Brand className="text-start m-0 p-0 " as={Link} to="/">
-            <img src={logo} alt="Logo" className="w-50 m-0 p-0" />
-          </Navbar.Brand>
+            <Navbar.Brand className="text-start m-0 p-0 " as={Link} to="/">
+              <img src={logo} alt="Logo" className="w-50 m-0 p-0" />
+            </Navbar.Brand>
           </Col>
 
-
           <Col lg={6} className="text-center pb-3">
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse>
+              <div className="text-light px-5 py-2 fuente fs-5 fw-bold">
+                {saludoNavbar}
+              </div>
 
-          <div className="text-light px-5 py-2">
-              {saludoNavbar}
-            </div>
- 
-            <Nav className="me-auto text-center">
-              <NavLink end className="nav-item nav-link" to="/">
-                Inicio
-              </NavLink>
-              {usuarioLogueado.email ? (
-                <>
-                <NavLink end className="nav-item nav-link" to="/administrador">
-              Administrar
-            </NavLink>
-            <NavLink end className="nav-item nav-link" to="/adminUsuarios">
-              Usuarios
-            </NavLink>
-                <Button variant="danger" onClick={logout}>
-                Logout
-              </Button>
-                </>
-              ) : (
-                <>
-                <NavLink end className="nav-item nav-link" to="/login">
-                Login
-              </NavLink>
-              <NavLink end className="nav-item nav-link" to="/registro">
-                Registrarse
-              </NavLink>
-                </>
-              )
-              }  
-            </Nav>
-            
-          </Navbar.Collapse>
+              <Nav className="me-auto text-center">
+                <NavLink end className="nav-item nav-link" to="/">
+                  Inicio
+                </NavLink>
+                <NavLink end className="nav-item nav-link" to="/registro">
+                  Registrarse
+                </NavLink>
+                {usuarioLogueado.email ? (
+                  <>
+                    <NavLink
+                      end
+                      className="nav-item nav-link"
+                      to="/administrador"
+                    >
+                      Administrar
+                    </NavLink>
+                    <NavLink
+                      end
+                      className="nav-item nav-link"
+                      to="/adminUsuarios"
+                    >
+                      Usuarios
+                    </NavLink>
+                    <Button variant="danger" onClick={logout}>
+                      Logout
+                    </Button>
+                  </>
+                ) : (
+                  <NavLink end className="nav-item nav-link" to="/login">
+                    Login
+                  </NavLink>
+                )}
+              </Nav>
+            </Navbar.Collapse>
           </Col>
         </Navbar>
       </Container>
