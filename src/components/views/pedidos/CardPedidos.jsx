3ade7f1f08afcaca56eb1baa213acfc354e60972
Pereyra Jose <luispereyra14@hotmail.com>
2022-11-25 20:52:27 -0300

    accedimos  al array solo nos queda renderizar todos los elementos

--- src/components/views/pedidos/CardPedidos.jsx
+++ src/components/views/pedidos/CardPedidos.jsx
@@ -6,14 +6,14 @@ import ListaPedidos from "./ListaPedidos";
 
 const CardPedidos = (props) => {
     
-  
+  console.log(props.pedidos.detallePedido.length)
   return (
 
     <Card className="bg-success text-light m-3" style={{ width: "18rem" }}>
       <Card.Header>ID:{props.pedidos._id}</Card.Header>
       <Card.Body>
         <Card.Title className="mb-2">Gerald the Ribia</Card.Title>
-        <Card.Text>{props.pedidos.detallePedido.map((pedido)=><ListaPedidos pedidos={props.pedidos}/>)}</Card.Text>
+        <Card.Text>{props.pedidos.detallePedido.map((pedido)=><ListaPedidos key={pedido._id} pedidos={props.pedidos}/>)}</Card.Text>
         <Card.Text>Estado: {props.pedidos.estado}</Card.Text>
         <Card.Text>Costo Total: ${props.pedidos.costoTotal}</Card.Text>
         <div className="d-flex justify-content-end">
