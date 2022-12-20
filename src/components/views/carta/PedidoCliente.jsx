07c5c0900b09e71bd6009939c5cfb4ab31c5d614
Mauricio-Alonso <conta.alonso@gmail.com>
2022-12-20 16:44:32 -0300

    quitado de la tabla de mis pedidos el codigo

--- src/components/views/carta/PedidoCliente.jsx
+++ src/components/views/carta/PedidoCliente.jsx
@@ -32,7 +32,7 @@ const AdministradorPedidos = () => {
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
-                <th>Código</th>
+                {/* <th>Código</th> */}
                 <th> Detalle Pedido</th>
                 <th> Estado</th>
                 <th> Precio Total</th>
